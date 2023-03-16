63e7817f40280b12cd6870ea26ab78698b25ac0b
Bruno Franco <brunoantunesfranco@gmail.com>
2023-03-16 17:57:42 +0100

    auto commit

--- srcs/utils/reset_game.c
+++ srcs/utils/reset_game.c
@@ -6,7 +6,7 @@
 /*   By: bfranco <bfranco@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/16 16:20:12 by bfranco       #+#    #+#                 */
-/*   Updated: 2023/03/16 17:39:54 by bfranco       ########   odam.nl         */
+/*   Updated: 2023/03/16 17:55:22 by bfranco       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ static void	reset_player(t_game *game, t_player *player)
 
 	img = player->img;
 	pos = player->start_pos;
+	ft_memcpy(player->pos, player->start_pos, sizeof(t_pos *));
 	img->enabled = false;
 	img->pixels = player->sprites[0][0];
 	img->instances[0].x = SIZE * pos->x + \
@@ -30,11 +31,37 @@ static void	reset_player(t_game *game, t_player *player)
 	img->enabled = true;
 }
 
+static void	reset_enemies(t_game *game, t_enemy **list)
+{
+	t_pos			*pos;
+	mlx_image_t		*img;
+	t_enemy			*head;
+
+	head = *list;
+	while (head)
+	{
+		img = head->img;
+		pos = head->start_pos;
+		ft_printf("%d %d\n", head->pos->x, head->pos->y);
+		ft_memcpy(head->pos, pos, sizeof(t_pos *));
+		img->enabled = false;
+		img->pixels = head->sprites[0];
+		img->instances[0].x = SIZE * pos->x + \
+		(game->width - game->map_info->cols * SIZE) / 2 - 155;
+		img->instances[0].y = SIZE * pos->y + \
+		(game->height - game->map_info->rows * SIZE) / 2 - 20;
+		head->killed = false;
+		head->dir = 3;
+		img->enabled = true;
+		head = head->next;
+	}
+}
+
 void	restart_game(t_game *game)
 {
 	ft_printf("RESTART!!!!!!!!");
 	reset_player(game, game->player);
-	// reset_enemies(game->enemies);
+	reset_enemies(game, game->enemies);
 	// reset_collectibles(game->collectibles);
 	// exit_game(game);
 	return ;
